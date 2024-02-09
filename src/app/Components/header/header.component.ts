8ab0e9312e4aad850b312f97b91b6de7fb2c76ed
Mohamed Samir <MohamedSamirAsaad2000@gmail.com>
2024-02-09 15:24:13 +0200

    Detect Screen Position

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Check if the screen is scrolled to the top
+    if (window.scrollY === 0) {
+      console.log('Scrolled to the top');
+      // Perform actions when the screen is at the top
+    } else {
+      console.log(document.documentElement.querySelector("#Nav"))
+      console.log('Not at the top');
+      // Perform actions when the screen is not at the top
+    }
+  }
 }
